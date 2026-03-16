8956fd7430b01c87972976eae9f02a83deab5570
Arshhasan <arshhasan2016@gmail.com>
2026-03-16 23:00:43 +0530

    intit

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -92,6 +92,7 @@ const Profile = ({ onLogout }) => {
       ),
       schedule: (
         <img
+          width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"
           src="/scheduleoff.png"
           alt="Schedule off"
         />
@@ -141,11 +142,15 @@ const Profile = ({ onLogout }) => {
         />
       ),
       learning: (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 2L2 7L12 12L22 7L12 2Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-          <path d="M2 17L12 22L22 17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-          <path d="M2 12L12 17L22 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
+        // <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        //   <path d="M12 2L2 7L12 12L22 7L12 2Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        //   <path d="M2 17L12 22L22 17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        //   <path d="M2 12L12 17L22 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        // </svg>
+        <img
+          src="/learningcenter.png"
+          alt="Learning centre"
+        />
       ),
       feedback: (
         <img
